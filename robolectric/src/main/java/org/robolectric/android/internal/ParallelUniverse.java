9875515ec6fb98fe590fdd6b466ef21a8fdc7120
Googler <noreply@google.com>
2018-11-26 17:12:05 -0800

    Project import generated by Copybara.
    PiperOrigin-RevId: 222913060

--- robolectric/src/main/java/org/robolectric/android/internal/ParallelUniverse.java
+++ robolectric/src/main/java/org/robolectric/android/internal/ParallelUniverse.java
@@ -160,19 +160,7 @@ public class ParallelUniverse implements ParallelUniverseInterface {
     // TempDirectory tempDirectory = RuntimeEnvironment.getTempDirectory();
     // packageInfo.setVolumeUuid(tempDirectory.createIfNotExists(packageInfo.packageName +
     // "-dataDir").toAbsolutePath().toString());
-    setUpPackageStorage(applicationInfo);
-
-    if (sdkConfig.getApiLevel() <= VERSION_CODES.KITKAT) {
-      String sourcePath = ReflectionHelpers.getField(parsedPackage, "mPath");
-      if (sourcePath == null) {
-        sourcePath = RuntimeEnvironment.getTempDirectory()
-            .createIfNotExists("sourceDir").toString();
-      }
-      applicationInfo.publicSourceDir = sourcePath;
-      applicationInfo.sourceDir = sourcePath;
-    } else {
-      applicationInfo.publicSourceDir = parsedPackage.codePath;
-    }
+    setUpPackageStorage(applicationInfo, parsedPackage);
 
     // Bit of a hack... Context.createPackageContext() is called before the application is created.
     // It calls through
@@ -414,30 +402,40 @@ public class ParallelUniverse implements ParallelUniverseInterface {
     return RuntimeEnvironment.application;
   }
 
-  private static void setUpPackageStorage(ApplicationInfo applicationInfo) {
-    TempDirectory tempDirectory = RuntimeEnvironment.getTempDirectory();
-    applicationInfo.sourceDir =
-        tempDirectory
-            .createIfNotExists(applicationInfo.packageName + "-sourceDir")
-            .toAbsolutePath()
-            .toString();
-    applicationInfo.publicSourceDir =
-        tempDirectory
-            .createIfNotExists(applicationInfo.packageName + "-publicSourceDir")
-            .toAbsolutePath()
-            .toString();
-    applicationInfo.dataDir =
-        tempDirectory
-            .createIfNotExists(applicationInfo.packageName + "-dataDir")
-            .toAbsolutePath()
-            .toString();
+  private void setUpPackageStorage(ApplicationInfo applicationInfo,
+      PackageParser.Package parsedPackage) {
+    if (RuntimeEnvironment.useLegacyResources()) {
+      applicationInfo.sourceDir =
+          createTempDir(applicationInfo.packageName + "-sourceDir");
+      applicationInfo.publicSourceDir =
+          createTempDir(applicationInfo.packageName + "-publicSourceDir");
+    } else {
+      if (sdkConfig.getApiLevel() <= VERSION_CODES.KITKAT) {
+        String sourcePath = ReflectionHelpers.getField(parsedPackage, "mPath");
+        if (sourcePath == null) {
+          sourcePath = createTempDir("sourceDir");
+        }
+        applicationInfo.publicSourceDir = sourcePath;
+        applicationInfo.sourceDir = sourcePath;
+      } else {
+        applicationInfo.publicSourceDir = parsedPackage.codePath;
+        applicationInfo.sourceDir = parsedPackage.codePath;
+      }
+    }
+
+    applicationInfo.dataDir = createTempDir(applicationInfo.packageName + "-dataDir");
 
     if (RuntimeEnvironment.getApiLevel() >= Build.VERSION_CODES.N) {
-      applicationInfo.credentialProtectedDataDir =
-          tempDirectory.createIfNotExists("userDataDir").toAbsolutePath().toString();
-      applicationInfo.deviceProtectedDataDir =
-          tempDirectory.createIfNotExists("deviceDataDir").toAbsolutePath().toString();
+      applicationInfo.credentialProtectedDataDir = createTempDir("userDataDir");
+      applicationInfo.deviceProtectedDataDir = createTempDir("deviceDataDir");
     }
+  }
+
+  private String createTempDir(String name) {
+    return RuntimeEnvironment.getTempDirectory()
+        .createIfNotExists(name)
+        .toAbsolutePath()
+        .toString();
   }
 
   // TODO move/replace this with packageManager
